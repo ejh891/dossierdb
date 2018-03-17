5c82df3b030e95c6db3447be97edadba81872f92
EJ Hammond <ehammond@vistaprint.com>
2018-03-17 10:23:41 -0400

    Wrap more things in data props

--- server.js
+++ server.js
@@ -32,101 +32,109 @@ Database.connect();
 /********************************************************************************************************************/
 
 app.post('/api/v1/persons',
-    handleAsyncErrors(async (req, res) => {
-        const person = req.body.data.person;
+  handleAsyncErrors(async (req, res) => {
+    const person = req.body.data.person;
 
-        const createdPerson = await new PersonModel(person).save();
+    const createdPerson = await new PersonModel(person).save();
 
-        res.status(201); // created
-        res.json({ 
-            created: true,
-            data: {
-                person: createdPerson 
-            }
-        });
-    })
+    res.status(201); // created
+    res.json({
+      created: true,
+      data: {
+        person: createdPerson
+      }
+    });
+  })
 );
 
-app.get('/api/v1/persons', 
-    addCacheHeaders(moment.duration({ days: 1 }).asSeconds()), 
-    handleAsyncErrors(async (req, res) => {
-        const persons = await PersonModel.find({}).lean();
+app.get('/api/v1/persons',
+  addCacheHeaders(moment.duration({ days: 1 }).asSeconds()),
+  handleAsyncErrors(async (req, res) => {
+    const persons = await PersonModel.find({}).lean();
 
-        res.json(persons);
-    })
+    res.json({
+      data: {
+        persons
+      }
+    });
+  })
 );
 
-app.get('/api/v1/persons/:id', 
-    addCacheHeaders(moment.duration({ days: 1 }).asSeconds()), 
-    handleAsyncErrors(async (req, res) => {
-        const id = req.params.id;
+app.get('/api/v1/persons/:id',
+  addCacheHeaders(moment.duration({ days: 1 }).asSeconds()),
+  handleAsyncErrors(async (req, res) => {
+    const id = req.params.id;
 
-        const person = await PersonModel.findOne({ _id: id }).lean();
+    const person = await PersonModel.findOne({ _id: id }).lean();
 
-        res.json(person);
+    res.json({
+      data: {
+        person
+      }
     })
+  })
 );
 
 app.put('/api/v1/persons/:id',
-    handleAsyncErrors(async (req, res) => {
-        const id = req.params.id;
-        const person = req.body.data.person;
+  handleAsyncErrors(async (req, res) => {
+    const id = req.params.id;
+    const person = req.body.data.person;
 
-        try {
-            const updatedPerson = await PersonModel.findOneAndUpdate(
-                { _id: id },
-                person,
-                { new: true } // return the updated doc instead of the original doc
-            );
-    
-            if (updatedPerson === null) {
-                throw boom.notFound(`Person with id: ${id} was not found`);
-            }
+    try {
+      const updatedPerson = await PersonModel.findOneAndUpdate(
+        { _id: id },
+        person,
+        { new: true } // return the updated doc instead of the original doc
+      );
 
-            res.json({ 
-                updated: true,
-                data: {
-                    person: updatedPerson 
-                }
-            });
-            return;
-        } catch (error) {
-            if (error instanceof mongoose.CastError) {
-                throw boom.badRequest('Id is malformed');
-            }
+      if (updatedPerson === null) {
+        throw boom.notFound(`Person with id: ${id} was not found`);
+      }
 
-            throw error;
+      res.json({
+        updated: true,
+        data: {
+          person: updatedPerson
         }
+      });
+      return;
+    } catch (error) {
+      if (error instanceof mongoose.CastError) {
+        throw boom.badRequest('Id is malformed');
+      }
 
-    })
+      throw error;
+    }
+
+  })
 );
 
 app.delete('/api/v1/persons/:id',
-    handleAsyncErrors(async (req, res) => {
-        const id = req.params.id;
+  handleAsyncErrors(async (req, res) => {
+    const id = req.params.id;
 
-        try {
-            const deletedPerson = await PersonModel.findOneAndRemove({_id: id});
-            
-            if (deletedPerson === null) {
-                throw boom.notFound(`Person with id: ${id} was not found`);
-            }
+    try {
+      const deletedPerson = await PersonModel.findOneAndRemove({ _id: id });
 
-            res.json({
-                deleted: true,
-                data: {
-                    person: deletedPerson
-                }
-            });
-            return;
-        } catch (error) {
-            if (error instanceof mongoose.CastError) {
-                throw boom.badRequest('Id is malformed');
-            }
+      if (deletedPerson === null) {
+        throw boom.notFound(`Person with id: ${id} was not found`);
+      }
 
-            throw error;
+      res.json({
+        deleted: true,
+        data: {
+          person: deletedPerson
         }
-    })
+      });
+      return;
+    } catch (error) {
+      if (error instanceof mongoose.CastError) {
+        throw boom.badRequest('Id is malformed');
+      }
+
+      throw error;
+    }
+  })
 );
 
 /**********************************************************************************************************************
@@ -134,66 +142,68 @@ app.delete('/api/v1/persons/:id',
 /********************************************************************************************************************/
 
 app.post('/api/v1/records',
-    handleAsyncErrors(async (req, res) => {
-        const record = req.body.data.record;
+  handleAsyncErrors(async (req, res) => {
+    const record = req.body.data.record;
 
-        const createdRecord = await new RecordModel(record).save();
+    const createdRecord = await new RecordModel(record).save();
 
-        res.status(201); // created
-        res.json({
-            created: true,
-            data: {
-                record: createdRecord 
-            }
-        });
-    })
+    res.status(201); // created
+    res.json({
+      created: true,
+      data: {
+        record: createdRecord
+      }
+    });
+  })
 );
 
 app.get('/api/v1/records',
-    addCacheHeaders(moment.duration({ days: 1 }).asSeconds()),
-    handleAsyncErrors(async (req, res) => {
-        const {
-            personId
-        } = req.query;
+  addCacheHeaders(moment.duration({ days: 1 }).asSeconds()),
+  handleAsyncErrors(async (req, res) => {
+    const {
+      personId
+    } = req.query;
 
-        const query = RecordModel.find().lean();
+    const query = RecordModel.find().lean();
 
-        if (personId) {
-            query.where('personId').equals(personId);
-        }
+    if (personId) {
+      query.where('personId').equals(personId);
+    }
 
-        const records = await query.exec();
+    const records = await query.exec();
 
-        res.json(records);
-    })
+    res.json({
+      data: records
+    });
+  })
 );
 
 app.delete('/api/v1/records/:id',
-    handleAsyncErrors(async (req, res) => {
-        const id = req.params.id;
+  handleAsyncErrors(async (req, res) => {
+    const id = req.params.id;
 
-        try {
-            const deletedRecord = await RecordModel.findOneAndRemove({_id: id});
-            
-            if (deletedRecord === null) {
-                throw boom.notFound(`Record with id: ${id} was not found`);
-            }
+    try {
+      const deletedRecord = await RecordModel.findOneAndRemove({ _id: id });
 
-            res.json({ 
-                deleted: true,
-                data: {
-                    record: deletedRecord
-                }
-            });
-            return;
-        } catch (error) {
-            if (error instanceof mongoose.CastError) {
-                throw boom.badRequest('Id is malformed');
-            }
+      if (deletedRecord === null) {
+        throw boom.notFound(`Record with id: ${id} was not found`);
+      }
 
-            throw error;
+      res.json({
+        deleted: true,
+        data: {
+          record: deletedRecord
         }
-    })
+      });
+      return;
+    } catch (error) {
+      if (error instanceof mongoose.CastError) {
+        throw boom.badRequest('Id is malformed');
+      }
+
+      throw error;
+    }
+  })
 );
 
 /**********************************************************************************************************************
@@ -202,30 +212,30 @@ app.delete('/api/v1/records/:id',
 
 // development error handler
 if (env.NODE_ENV === 'development') {
-    app.use((err, req, res, next) => {
-        if (!err.isBoom) {
-            boom.boomify(err);
-        }
-
-        res.status(err.output.statusCode || 500);
-        res.json({
-            message: err.message,
-            error: err
-        });
-    });
-}
-  
-// production error handler
-app.use((err, req, res, next) => {
+  app.use((err, req, res, next) => {
     if (!err.isBoom) {
-        boom.boomify(err);
+      boom.boomify(err);
     }
 
     res.status(err.output.statusCode || 500);
     res.json({
-        message: err.message,
-        error: {} // don't leak stack trace
+      message: err.message,
+      error: err
     });
+  });
+}
+
+// production error handler
+app.use((err, req, res, next) => {
+  if (!err.isBoom) {
+    boom.boomify(err);
+  }
+
+  res.status(err.output.statusCode || 500);
+  res.json({
+    message: err.message,
+    error: {} // don't leak stack trace
+  });
 });
 
 /**********************************************************************************************************************
@@ -233,5 +243,5 @@ app.use((err, req, res, next) => {
 /********************************************************************************************************************/
 
 httpServer.listen(port, () => {
-    console.log('Api running on port: ' + port);
+  console.log('Api running on port: ' + port);
 });
